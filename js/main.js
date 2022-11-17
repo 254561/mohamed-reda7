995d56928c520bc0d739553b43d52d6ba227798f
mohamedreda1258 <mohamed reda.com>
2022-11-17 17:49:20 +0200

    v7

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel();
+});
+
+
+
+
+new WOW().init();
+
+
+new AutoTyping('.autotext' [
+    'احداث واقوي الملابس الرياضيه  ',
+    'كل جديد عندنا وبس',
+    'هدفنا ارضائكم دائما',
+    '💻'
+
+], {
+    waitBeforeDelete: 1000,
+    waitBetweenWords: 10,
+    writeWhole: true
+}).start();
+
+
+
+
+const typing = new AutoTyping('.autotext', [
+    'احداث واقوي الملابس الرياضيه',
+    'كل جديد عندنا وبس',
+    'هدفنا ارضائكم دائما'
+], );
+typing.start();
